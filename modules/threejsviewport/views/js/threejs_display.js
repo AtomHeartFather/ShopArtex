faf8e8180fb838342d06f24eee55b23e4b5809fa
Егор Якимов <ea.yakimov@gmail.com>
2019-06-14 23:25:51 +0300

    пгомежуточный

--- modules/threejsviewport/views/js/threejs_display.js
+++ modules/threejsviewport/views/js/threejs_display.js
@@ -66,62 +66,67 @@ function init() {
     scene.add(meshFloor);
 
     // загрузчик модели
-    var loader = new THREE.GLTFLoader();
-
+    var loader4 = new THREE.GLTFLoader();
     // грузануть glTF
-    loader.load(
+    loader4.load(
             // путь
             'modules/threejsviewport/views/models_3d/4.gltf',
             // вызываеться, когда модель загружена
                     function (gltf) {
-                        /*  var bb = new THREE.Box3()
-                         bb.setFromObject(gltf.scene);
-                         bb.center(controls.target);
-                         controls.update(); */
                         const myModel = gltf.scene;
-                        
-                        //перебирает ВСЕ поверхности модели и говорит им отбрасывать и рисовать тени(но не работает)
-                        myModel.traverse(function (child) {
-                            if (child.isMesh) {
-                                child.castShadow = true;
-                                child.receiveShadow = true;
-                            }
-                        });
-
                         // ресайз объекта до максимального размера в еденицу и постановка его в центр координат
                         var mroot = gltf.scene;
                         var bbox = new THREE.Box3().setFromObject(mroot);
-                        //var cent = bbox.getCenter(new THREE.Vector3());
                         var size = bbox.getSize(new THREE.Vector3());
                         //Rescale the object to normalized space
                         var maxAxis = Math.max(size.x, size.y, size.z);
                         mroot.scale.multiplyScalar(1.0 / maxAxis);
                         bbox.setFromObject(mroot);
-                        //bbox.getCenter(cent);
-                        //bbox.center(controls.target);
-                        //controls.update();
                         bbox.getSize(size);
-                        //Reposition to 0,halfY,0
-                        //mroot.position.copy(cent).multiplyScalar(-1);
                         mroot.position.y -= (size.y * 0.5);
-                        scene.add(gltf.scene);
+                        scene.add(mroot);
                     },
-                    
                             // выполняеться пока все еще не загрузилось
-                            function (xhr) {
-                                console.log((xhr.loaded / xhr.total * 100) + '% loaded');
-                            },
+                            function (xhr) {console.log((xhr.loaded / xhr.total * 100) + '% loaded');},
                             // когда есть ошибки
-                                    function (error) {
-                                        console.log('An error happened');
-                                    }
+                            function (error) {console.log('An error happened');
+                            }
                 );
-                            /*geometry = new THREE.BoxGeometry( 0.2, 0.2, 0.2 );
-                             material = new THREE.MeshNormalMaterial();
-                             mesh = new THREE.Mesh( geometry, material );
-                             scene.add( mesh );*/
-                            //renderer = new THREE.WebGLRenderer({ alpha: true });
-                            
+
+    // загрузчик модели
+    var loader5 = new THREE.GLTFLoader();
+    // грузануть glTF
+    loader5.load(
+            // путь
+            'modules/threejsviewport/views/models_3d/5.gltf',
+            // вызываеться, когда модель загружена
+                    function (gltf) {
+                        const myModel = gltf.scene;
+                        // ресайз объекта до максимального размера в еденицу и постановка его в центр координат
+                        var mroot = gltf.scene;
+                        var bbox = new THREE.Box3().setFromObject(mroot);
+                        var size = bbox.getSize(new THREE.Vector3());
+                        //Rescale the object to normalized space
+                        var maxAxis = Math.max(size.x, size.y, size.z);
+                        mroot.scale.multiplyScalar(1.0 / maxAxis);
+                        bbox.setFromObject(mroot);
+                        bbox.getSize(size);
+                        mroot.position.y -= (size.y * 0.5);
+                        scene.add(mroot);
+                    },
+                            // выполняеться пока все еще не загрузилось
+                            function (xhr) {console.log((xhr.loaded / xhr.total * 100) + '% loaded');},
+                            // когда есть ошибки
+                            function (error) {console.log('An error happened');
+                            }
+                );
+
+    document.getElementById("clored").addEventListener("click", function(){
+        alert('Добрый день');
+//        scene.remove(currentSphere);
+//        scene.add(nextSphere);
+    });
+
                             //блок конечной отрисовки
                             renderer = new THREE.WebGLRenderer({antialias: true, alpha: true});//сглаживание пикселизации, включение прозрачности
                             //renderer.setClearColor(0xEEEEEE);
@@ -139,22 +144,15 @@ function init() {
                             document.getElementById('3dClo').appendChild(renderer.domElement); //куда в HTML пихать экран отрисовки
                             //window.addEventListener( 'resize', onWindowResize, false ); //для адаптивности, событие при изменении размера экрана в реальном времени
                         }
-// 1ый вариант функции для адаптивности 
-// contenedor.clientWidth / contenedor.clientHeight; -это типа размеры берущиеся прям у бразуера пользователя
-// window.innerWidth, window.innerHeight  - это какойто более стандартный способ взятия размеров экрана
-//function onWindowResize() {
-//camera.aspect = contenedor.clientWidth / contenedor.clientHeight; //тут берут размер экрана У ПОЛЬЗОВАТЕЛЯ
-//camera.updateProjectionMatrix();
-//renderer.setSize(contenedor.clientWidth, contenedor.clientHeight);
-//}
+
 
     //функция финального рендера и для анимации
-    function animate() {
+function animate() {
     resizeCanvasToDisplaySize();
     requestAnimationFrame(animate);
     renderer.render(scene, camera);
     document.getElementsByClassName('images-container')[0].style.display = 'none';
-    }
+}
 
 function resizeCanvasToDisplaySize() {
   const canvas = renderer.domElement;
@@ -168,11 +166,10 @@ function resizeCanvasToDisplaySize() {
     renderer.setSize(width, height, false);
     camera.aspect = width / height;
     camera.updateProjectionMatrix();
-
     // update any render target sizes here
   }
 }
 
-clored.onclick = getreddress() {
-    
-  };
+//clored.onclick = getreddress() {
+//    
+//  };
