8e4cddab10ff08b31690e95cdf7d553bfc04c9fd
Егор Якимов <ea.yakimov@gmail.com>
2019-06-28 22:43:49 +0300

    успешное_добавление_каталога_темы

---
+++ themes/artex/assets/js/custom.js
@@ -0,0 +1,64 @@
+/**
+ * 2007-2017 PrestaShop
+ *
+ * NOTICE OF LICENSE
+ *
+ * This source file is subject to the Academic Free License 3.0 (AFL-3.0)
+ * that is bundled with this package in the file LICENSE.txt.
+ * It is also available through the world-wide-web at this URL:
+ * https://opensource.org/licenses/AFL-3.0
+ * If you did not receive a copy of the license and are unable to
+ * obtain it through the world-wide-web, please send an email
+ * to license@prestashop.com so we can send you a copy immediately.
+ *
+ * DISCLAIMER
+ *
+ * Do not edit or add to this file if you wish to upgrade PrestaShop to newer
+ * versions in the future. If you wish to customize PrestaShop for your
+ * needs please refer to http://www.prestashop.com for more information.
+ *
+ * @author    PrestaShop SA <contact@prestashop.com>
+ * @copyright 2007-2017 PrestaShop SA
+ * @license   https://opensource.org/licenses/AFL-3.0 Academic Free License 3.0 (AFL-3.0)
+ * International Registered Trademark & Property of PrestaShop SA
+ */
+    
+function getQueryVariable(variable)
+{
+       var query = window.location.search.substring(1);
+       var vars = query.split("&");
+       for (var i=0;i<vars.length;i++) {
+               var pair = vars[i].split("=");
+               if(pair[0] == variable){return pair[1];}
+       }
+       return(false);
+}
+    
+  
+    
+  $(document).ready(function() {
+     
+     if ( getQueryVariable("id_product") == 21 ) {
+        return;
+        } 
+     
+    $('#lightSlider').lightSlider({
+        autoWidth:true,
+        adaptiveHeight:false,
+        item:1.5,
+        slideMargin:900, // расстояние между слайдерами
+        pager: false,
+        loop:true
+    });
+});
+
+//  $(document).ready(function() {
+//    $('#lightSlider2').lightSlider({
+//        autoWidth:true,
+//        adaptiveHeight:false,
+//        item:4,
+//        slideMargin:0, // расстояние между слайдерами
+//        pager: false,
+//        loop:true
+//    });
+//});
---
+++ themes/artex/assets/js/threejs_display.js
@@ -0,0 +1,167 @@
+
+var camera, scene, renderer;
+var geometry, material, mesh;
+var meshFloor;
+var USE_WIREFRAME = false;
+var modelpath;
+var mdl;
+
+var div = document.createElement('div');
+div.id = '3dClo';
+div.style.height = '100vh';
+div.style.display = 'flex';
+document.getElementById("content").insertBefore(div, document.getElementsByClassName("product-flags")[0]);
+
+var modelpath = "themes/artex/assets/3d/4.gltf";
+
+    document.getElementById("clored").addEventListener("click", function(){
+    modelpath = "themes/artex/assets/3d/4.gltf";
+    init(modelpath);
+    animate();
+    });
+    document.getElementById("clowhite").addEventListener("click", function(){
+    modelpath = "themes/artex/assets/3d/5.gltf";
+    init(modelpath);
+    animate();
+    });
+
+
+
+function init(modelpath) {
+    //мягкий окружающий свет
+    Light = new THREE.AmbientLight(0x888888); //цвет
+    
+    //точечный источник света
+    pointLight = new THREE.PointLight(0xffffff, 1.5, 18); //цвет, интенсивность, 3ий хз
+    pointLight.position.set(0, 3, 1); //позиция
+    pointLight.castShadow = true; //отбрасывает ли тени
+    pointLight.shadow.camera.near = 0.1; //ближняя граница просчитывания
+    pointLight.shadow.camera.far = 100; //дальняя граница просчитывания
+
+    //камера
+    camera = new THREE.PerspectiveCamera(50, window.innerWidth / window.innerHeight, 0.01, 4000); //глубина резкости, пропорции, ближняя граница просчитывания, и дальняя
+    //camera = new THREE.PerspectiveCamera(50, 88 , 0.01, 4000);
+    camera.position.set(1, 1, 1); //позиция
+    camera.lookAt(new THREE.Vector3(0, 0, 0)); //куда камера смотрит
+    
+    //крутилка модели
+    controls = new THREE.OrbitControls(camera, document.getElementById('3dClo'));
+    controls.minPolarAngle = 1.4; //минимально возможное вращение по вертикали
+    controls.maxPolarAngle = 1.4; //максимально возможное кручение по горизонтали
+    controls.enablePan = false; //можно ли перетаскивать камеру мышкой
+
+    //controls.target.set( -3000, 5000, 9000 );
+    controls.update();
+ 
+    //сцену создает
+    scene = new THREE.Scene();
+    
+    //добавляет в сцену истояники света
+    scene.add(Light);
+    scene.add(pointLight);
+    
+    //материал для искусственной тени под моделью
+    var mat = new THREE.MeshPhongMaterial();
+    mat.map = new THREE.ImageUtils.loadTexture("themes/artex/assets/img/gray+.png");//путь к картинке
+    mat.transparent = true;//есть ли прозрачность
+    mat.side = THREE.DoubleSide; //двухсторонняя ли
+    mat.depthWrite = false; //хз
+    //mat.color = new THREE.Color(0xff0000);
+    
+    //плоскость для искусственной тени
+    meshFloor = new THREE.Mesh(
+            new THREE.PlaneGeometry(0.7, 0.7, 0.7, 0.7),//размер х, размер у, размер полигональности по оси х, тоже по оси у
+            mat //какой материал поверхности
+            );
+    meshFloor.rotation.x -= Math.PI / 2; //повернуть плоскость на 90 градусов(а то по умолчанию стоит вертикально)
+    meshFloor.position.set(0, -0.5, 0); //опустить плоскость на 0.5(под ноги модели)
+    meshFloor.receiveShadow = true; //рисовать ли тени на плоскости
+    
+    //добавить плоскость тени в сцену
+    //scene.add(meshFloor);
+    
+
+
+//    
+//    function mymodelloader(modelpath) {    
+        
+    // загрузчик модели
+    var loader4 = new THREE.GLTFLoader();
+    // грузануть glTF
+    loader4.load(
+            // путь
+            //'modules/threejsviewport/views/models_3d/4.gltf',
+            modelpath,
+            // вызываеться, когда модель загружена
+                    function (gltf) {
+                        const myModel = gltf.scene;
+                        // ресайз объекта до максимального размера в еденицу и постановка его в центр координат
+                        var mroot = gltf.scene;
+                        var bbox = new THREE.Box3().setFromObject(mroot);
+                        var size = bbox.getSize(new THREE.Vector3());
+                        //Rescale the object to normalized space
+                        var maxAxis = Math.max(size.x, size.y, size.z);
+                        mroot.scale.multiplyScalar(1.0 / maxAxis);
+                        bbox.setFromObject(mroot);
+                        bbox.getSize(size);
+                        mroot.position.y -= (size.y * 0.5);
+                        scene.add(mroot);
+                    },
+                    // выполняеться пока все еще не загрузилось
+                            function (xhr) {console.log((xhr.loaded / xhr.total * 100) + '% loaded');},
+                            // когда есть ошибки
+                            function (error) {console.log('An error happened');
+                            }
+                );
+//        }        
+        
+// mymodelloader(modelpath);
+  
+
+
+                            //блок конечной отрисовки
+                            renderer = new THREE.WebGLRenderer({antialias: true, alpha: true});//сглаживание пикселизации, включение прозрачности
+                            //renderer.setClearColor(0xEEEEEE);
+                            renderer.setClearColor(0xaaaaaa, 0.5);//цвет фона, прозрачность от 0 до 1
+                            renderer.shadowMap.enable = true; //включены ли тени
+                            renderer.shadowMap.type = THREE.BasicShadowMap; //выбор типа теней
+                            renderer.gammaOutput = true; //гамма-коррекция
+                            renderer.gammaFactor = 2.2;  //какая гамма
+                            //renderer.setPixelRatio( window.devicePixelRatio );
+                            //document.body.appendChild( renderer.domElement );
+                            renderer.domElement.style.width = "100%";
+                            renderer.domElement.style.height = "100%";
+                            //renderer.setSize(window.innerWidth, window.innerHeight); //размер экрана отрисовки
+                            document.getElementById('3dClo').innerHTML = "";                                             
+                            document.getElementById('3dClo').appendChild(renderer.domElement); //куда в HTML пихать экран отрисовки
+                            //window.addEventListener( 'resize', onWindowResize, false ); //для адаптивности, событие при изменении размера экрана в реальном времени
+                        }
+
+
+//функция финального рендера и для анимации
+function animate() {
+    resizeCanvasToDisplaySize();
+    requestAnimationFrame(animate);
+    renderer.render(scene, camera);
+    document.getElementsByClassName('images-container')[0].style.display = 'none';
+}
+
+function resizeCanvasToDisplaySize() {
+  const canvas = renderer.domElement;
+  // look up the size the canvas is being displayed
+  const width = canvas.clientWidth;
+  const height = canvas.clientHeight;
+
+  // adjust displayBuffer size to match
+  if (canvas.width !== width || canvas.height !== height) {
+    // you must pass false here or three.js sadly fights the browser
+    renderer.setSize(width, height, false);
+    camera.aspect = width / height;
+    camera.updateProjectionMatrix();
+    // update any render target sizes here
+  }
+}
+
+//clored.onclick = getreddress() {
+//    
+//  };
